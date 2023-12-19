00d9bba6d2f57680d65d82f23ee778f6a8a7ac7e
zhyea <zhangrui@zhyea.com>
2023-12-19 14:06:54 +0800

    1. 调整反射相关工具类

--- common-utils/src/main/java/org/chobit/commons/utils/ReflectKit.java
+++ common-utils/src/main/java/org/chobit/commons/utils/ReflectKit.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
@@ -21,6 +22,51 @@ import java.util.stream.Collectors;
 public final class ReflectKit {
 
     private static final Logger logger = LoggerFactory.getLogger(ReflectKit.class);
+
+
+    /**
+     * synthetic方法修饰符int值
+     */
+    private static final int SYNTHETIC = 0x00001000;
+
+
+    /**
+     * 判断是否是default方法
+     *
+     * @param method 方法名
+     * @return true 是default方法， false 不是default方法
+     */
+    public static boolean isDefault(Method method) {
+
+        Class<?> clazz = method.getDeclaringClass();
+
+        // 只有接口中有default方法
+        if (!clazz.isInterface()) {
+            return false;
+        }
+
+        // Default methods are public non-abstract, non-synthetic, and non-static instance methods
+        // declared in an interface.
+        // method.isDefault() is not sufficient for our usage as it does not check
+        // for synthetic methods. As a result, it picks up overridden methods as well as actual default
+        // methods.
+        return (
+                (method.getModifiers()
+                        & (Modifier.ABSTRACT | Modifier.PUBLIC | Modifier.STATIC | SYNTHETIC))
+                        == Modifier.PUBLIC
+        );
+    }
+
+
+    /**
+     * 判断是否是静态方法
+     *
+     * @param method 待分析方法
+     * @return true 是静态方法， false 不是静态方法
+     */
+    public static boolean isStatic(Method method) {
+        return (method.getModifiers() & Modifier.STATIC) != 0;
+    }
 
 
     /**
