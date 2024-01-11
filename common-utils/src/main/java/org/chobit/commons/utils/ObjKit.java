37504199a4542a7685619e7b542048f6e1596c69
zhyea <zhangrui@zhyea.com>
2024-01-12 00:29:24 +0800

    1. 添加ObjKit对象工具类
    Change-Id: I888a9a4e3741390524a5ed913932b7fb5db69722

---
+++ common-utils/src/main/java/org/chobit/commons/utils/ObjKit.java
@@ -0,0 +1,29 @@
+package org.chobit.commons.utils;
+
+import java.util.Objects;
+
+/**
+ * 对象操作工具类
+ *
+ * @author robin
+ */
+public final class ObjKit {
+
+
+    /**
+     * 判断两个对象是否不相等
+     *
+     * @param a 对象a
+     * @param b 对象b
+     * @return true 不相等， false 相等
+     */
+    public static boolean nonEquals(final Object a, final Object b) {
+        return !Objects.equals(a, b);
+    }
+
+
+    private ObjKit() {
+        throw new UnsupportedOperationException("Private constructor, cannot be accessed.");
+    }
+
+}
